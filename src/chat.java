2ce111a46c8bed8809c0b53b57c26e8d47790323
IgnacioAstuaR <iastua@estudiantec.cr>
2023-08-20 21:45:32 -0600

    Interfaz del Chat

---
+++ src/chat.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class chat {
+    public static void main(String[] args) {
+        ventana ventana1 = new ventana();
+        ventana1.setVisible(true);
+
+    }
+}
+class ventana extends JFrame{
+    public ventana() {
+        setSize(400, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setLocation(150, 50);
+        setTitle("Chat");
+        panel panel1 = new panel();
+        add(panel1);
+    }
+}
+class panel extends JPanel implements ActionListener{
+    JLabel texto1 = new JLabel("Mensaje");
+    JTextField txt = new JTextField(35);
+    JButton enviar = new JButton("Enviar");
+    public panel(){
+        add(texto1);
+        add(txt);
+        add(enviar);
+        enviar.addActionListener(this);
+    }
+    public void actionPerformed(ActionEvent e) {
+        System.out.print("Envio");
+    }
+}
