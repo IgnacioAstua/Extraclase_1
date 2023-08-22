b7b2581449051df04e0f734bfb3067b83221b2cb
JoSePhPM23 <128115332+JoSePhPM23@users.noreply.github.com>
2023-08-21 19:48:20 -0600

    Correción
    Error server

--- src/chat.java
+++ src/chat.java
@@ -4,44 +4,45 @@ import java.awt.event.ActionListener;
 import java.io.*;
 import java.net.*;
 
-//Joseph
 public class chat {
     public static void main(String[] args) {
-        ventana ventana1 = new ventana();
+        Ventana ventana1 = new Ventana();
         ventana1.setVisible(true);
-
     }
 }
-class ventana extends JFrame{
-    public ventana() {
+
+class Ventana extends JFrame {
+    public Ventana() {
         setSize(400, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
         setLocation(150, 50);
         setTitle("Chat");
-        panel panel1 = new panel();
+        Panel panel1 = new Panel();
         add(panel1);
     }
 }
-class panel extends JPanel implements ActionListener{
+
+class Panel extends JPanel implements ActionListener {
     JLabel texto1 = new JLabel("Mensaje");
     JTextField txt = new JTextField(35);
     JButton enviar = new JButton("Enviar");
 
-    Socket socket;
-    BufferedReader reader;
-    PrintWriter writer;
+    ServerSocket serverSocket; // El socket del servidor
+    Socket socket;  // El socket para la comunicación con el cliente
+    BufferedReader reader;  // Para leer datos del cliente
+    PrintWriter writer;  // Para escribir datos al cliente
 
-    public panel(){
+    public Panel() {
         add(texto1);
         add(txt);
         add(enviar);
         enviar.addActionListener(this);
 
-        // Inicializa el servidor aparte
+        // Inicializar el servidor en un hilo aparte
         new Thread(() -> {
             try {
-                ServerSocket serverSocket = new ServerSocket(0); // Elige un puerto disponible
+                serverSocket = new ServerSocket(0); // Elige un puerto disponible
                 System.out.println("Servidor escuchando en el puerto: " + serverSocket.getLocalPort());
 
                 while (true) {
@@ -60,14 +61,25 @@ class panel extends JPanel implements ActionListener{
         try {
             String message = txt.getText();
 
-            // Envia el mensaje al servidor
+            // Enviar el mensaje al servidor
             if (writer != null) {
                 writer.println(message);
             }
 
-            txt.setText(""); // Limpia el campo de texto
+            txt.setText(""); // Limpiar el campo de texto
         } catch (Exception ex) {
             ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void finalize() {
+        try {
+            if (serverSocket != null) {
+                serverSocket.close(); // Cierra el socket del servidor al finalizar
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
