792a5db2216f52da0bedcec92396b38dae66c77e
JoSePhPM23 <128115332+JoSePhPM23@users.noreply.github.com>
2023-08-23 11:08:53 -0600

    avance

--- src/servidor.java
+++ src/servidor.java
@@ -5,3 +5,12 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
+private Socket conexion;
+private ServerSocket servidor;
+private ObjectInputStream entrada;
+private ObjectOutputStream salida;
+private int contador = 1;
+
+private void publicar_mensaje (final String muestra_mensaje) {
+    SwingUtilities.invokeLater
+}
