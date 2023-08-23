d7bacef6e648078e750ef97109d48ed85b115f6b
JoSePhPM23 <128115332+JoSePhPM23@users.noreply.github.com>
2023-08-23 11:43:38 -0600

    Update servidor.java

--- src/servidor.java
+++ src/servidor.java
@@ -10,7 +10,7 @@ private Socket conexion;
 private ServerSocket servidor;
 private ObjectInputStream entrada;
 private ObjectOutputStream salida;
-private int contador = 1;
+private int i = 1;
 
 private void publicar_mensaje (final String muestra_mensaje) {
     SwingUtilities.invokeLater(
@@ -19,4 +19,30 @@ private void publicar_mensaje (final String muestra_mensaje) {
                 pantalla.append(muestra_mensaje);
             }
         } );
+}
+
+public void encender_server() {
+    try {
+        servidor = new ServerSocket(12344, 100);
+        while (true) {
+            try {
+                espera_conexion();
+                obtener_flujos();
+                procesa_conexion();
+            } catch (EOFException e) {
+                publicar_mensaje("\nSe ha terminado la conexión");
+            } finally {
+                cierra_conexion();
+                i++;
+            }
+        }
+    } catch (IOException excepcion) {
+        excepcion.printStackTrace();
+    }
+}
+
+private void espera_conexion() throws IOException {
+    publicar_mensaje("Esperando conexión\n");
+    conexion = servidor.accept();
+    publicar_mensaje("Conexion " + i + " obtenida de: " + conexion.getInetAddress().getHostAddress());
 }
