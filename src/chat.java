79564b1ff08b673c419dc8a1f97d7482a1a1f466
JoSePhPM23 <128115332+JoSePhPM23@users.noreply.github.com>
2023-08-25 12:42:07 -0600

    borra campo texto
    Elimina el contenido del campo de texto después de enviar el mensaje

--- src/chat.java
+++ src/chat.java
@@ -8,55 +8,58 @@ import java.net.*;
 
 public class chat {
     public static void main(String[] args) {
-        Ventana ventana1 = new Ventana();
-        ventana1.setVisible(true);
+        SwingUtilities.invokeLater(() -> {
+            Ventana ventana = new Ventana();
+            ventana.setVisible(true);
+        });
     }
 }
 
 class Ventana extends JFrame {
-    private Panel panel1;
+    private Panel panel;
 
     public Ventana() {
         setSize(400, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
         setLocation(150, 50);
-        setTitle("Chat1");
-        panel1 = new Panel();
-        add(panel1);
+        setTitle("Chat");
+        panel = new Panel();
+        add(panel);
     }
 }
 
 class Panel extends JPanel implements Runnable {
-    JTextField txt = new JTextField(35);
-    JTextField ip = new JTextField(35);
-    JTextField userName = new JTextField(15);
-    JTextArea txt_area = new JTextArea(20, 35);
-    JLabel portLabel = new JLabel("Puerto:"); // Etiqueta para mostrar el puerto
+    private JTextField txt = new JTextField(35);
+    private JTextField ip = new JTextField(35);
+    private JTextField userName = new JTextField(15);
+    private JTextArea txt_area = new JTextArea(20, 35);
+    private JLabel portLabel = new JLabel("Puerto:");
 
     public Panel() {
-        JLabel Ip = new JLabel("IP:");
-        add(Ip);
+        JLabel ipLabel = new JLabel("IP:");
+        add(ipLabel);
         add(ip);
-        
+
         JLabel nameLabel = new JLabel("Nombre:");
         add(nameLabel);
         add(userName);
-        
+
         JLabel sms = new JLabel("Mensajes:");
         add(sms);
         txt_area.setEditable(false);
         add(txt_area);
         add(txt);
-        
+
         JButton enviar = new JButton("Enviar");
         Envia evento = new Envia();
         enviar.addActionListener(evento);
         add(enviar);
-        
-        add(portLabel); // Agregar la etiqueta del puerto
-        portLabel.setText("Puerto: " + 9999); // Mostrar el puerto inicial
-        
+
+        add(portLabel);
+        int randomPort = (int) (Math.random() * 10000) + 10000;
+        portLabel.setText("Puerto: " + randomPort);
+
         Thread hilo1 = new Thread(this);
         hilo1.start();
     }
@@ -67,10 +70,11 @@ class Panel extends JPanel implements Runnable {
             String IP = ip.getText();
             String mensaje = userName.getText() + ": " + txt.getText();
             try {
-                Socket misocket = new Socket(IP, 9999);//SOCKET
+                Socket misocket = new Socket(IP, 9999);
                 DataOutputStream salida = new DataOutputStream(misocket.getOutputStream());
                 salida.writeUTF(mensaje);
                 salida.close();
+                txt.setText(""); // Elimina el contenido del campo de texto después de enviar el mensaje
             } catch (UnknownHostException e1) {
                 e1.printStackTrace();
             } catch (IOException e1) {
@@ -83,7 +87,7 @@ class Panel extends JPanel implements Runnable {
     public void run() {
         try {
             ServerSocket servidor = new ServerSocket(9999);
-            portLabel.setText("Puerto: " + servidor.getLocalPort()); // Actualizar la etiqueta con el puerto real
+            portLabel.setText("Puerto: " + servidor.getLocalPort());
             while (true) {
                 Socket misocket = servidor.accept();
                 DataInputStream entrada = new DataInputStream(misocket.getInputStream());
