9843a5c4cf1cc579b088d3c71a1fdb3ea23f67fb
JoSePhPM23 <128115332+JoSePhPM23@users.noreply.github.com>
2023-08-25 10:57:10 -0600

    CAMBIOS

--- src/chat.java
+++ src/chat.java
@@ -1,37 +1,107 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.*;
 
 public class chat {
     public static void main(String[] args) {
-        ventana ventana1 = new ventana();
+        Ventana ventana1 = new Ventana();
         ventana1.setVisible(true);
-
     }
 }
-class ventana extends JFrame{
-    public ventana() {
+
+class Ventana extends JFrame {
+    private Panel panel1;
+
+    public Ventana() {
         setSize(400, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
         setLocation(150, 50);
-        setTitle("Chat");
-        panel panel1 = new panel();
+        setTitle("Chat1");
+        panel1 = new Panel();
         add(panel1);
     }
 }
-class panel extends JPanel implements ActionListener{
-    JLabel texto1 = new JLabel("Mensaje");
+
+class Panel extends JPanel implements Runnable {
     JTextField txt = new JTextField(35);
-    JButton enviar = new JButton("Enviar");
-    public panel(){
-        add(texto1);
+    JTextField ip = new JTextField(35);
+    JTextField userName = new JTextField(15);
+    JTextArea txt_area = new JTextArea(20, 35);
+    JLabel portLabel = new JLabel("Puerto:"); // Etiqueta para mostrar el puerto
+
+    public Panel() {
+        JLabel Ip = new JLabel("IP:");
+        add(Ip);
+        add(ip);
+        
+        JLabel nameLabel = new JLabel("Nombre:");
+        add(nameLabel);
+        add(userName);
+        
+        JLabel sms = new JLabel("Mensajes:");
+        add(sms);
+        txt_area.setEditable(false);
+        add(txt_area);
         add(txt);
+        
+        JButton enviar = new JButton("Enviar");
+        Envia evento = new Envia();
+        enviar.addActionListener(evento);
         add(enviar);
-        enviar.addActionListener(this);
+        
+        add(portLabel); // Agregar la etiqueta del puerto
+        portLabel.setText("Puerto: " + 9999); // Mostrar el puerto inicial
+        
+        Thread hilo1 = new Thread(this);
+        hilo1.start();
     }
-    public void actionPerformed(ActionEvent e) {
-        System.out.print("Enviar");
+
+    private class Envia implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String IP = ip.getText();
+            String mensaje = userName.getText() + ": " + txt.getText();
+            try {
+                Socket misocket = new Socket(IP, 9999);
+                DataOutputStream salida = new DataOutputStream(misocket.getOutputStream());
+                salida.writeUTF(mensaje);
+                salida.close();
+            } catch (UnknownHostException e1) {
+                e1.printStackTrace();
+            } catch (IOException e1) {
+                System.out.println(e1.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            ServerSocket servidor = new ServerSocket(9999);
+            portLabel.setText("Puerto: " + servidor.getLocalPort()); // Actualizar la etiqueta con el puerto real
+            while (true) {
+                Socket misocket = servidor.accept();
+                DataInputStream entrada = new DataInputStream(misocket.getInputStream());
+                String mensaje = entrada.readUTF();
+                txt_area.append("\n" + mensaje);
+                misocket.close();
+            }
+        } catch (IOException e1) {
+            throw new RuntimeException(e1);
+        }
     }
 }
+
+
+
+
+
+
+
+
+
